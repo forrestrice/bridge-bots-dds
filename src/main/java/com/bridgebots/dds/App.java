1378f661856c628fbd6f8953de099160c5339233
forrestrice <forrest.d.rice@gmail.com>
2019-09-29 18:49:00 -0700

    inital commit

---
+++ src/main/java/com/bridgebots/dds/App.java
@@ -0,0 +1,13 @@
+package com.bridgebots.dds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
